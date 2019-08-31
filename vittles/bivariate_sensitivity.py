a192d87600959656205fb631b60ecf0c8167acb5
Ryan Giordano <rgiordan@gmail.com>
2019-08-31 10:06:17 -0700

    basic tests

--- vittles/bivariate_sensitivity.py
+++ vittles/bivariate_sensitivity.py
@@ -1,3 +1,5 @@
+import autograd
+from autograd import numpy as np
 from .sensitivity_lib import _append_jvp
 from copy import deepcopy
 import warnings
@@ -117,21 +119,38 @@ class OptimumChecker():
                  solver,
                  input_base,
                  hyper_base):
+        """Estimate the error in sensitivity due to incomplete optimization.
+
+        Parameters
+        ---------------
+        estimating_equation : `callable`
+            A function taking arguments `(input, hyper)` and returning
+            a vector, typically the same length as the input.  The idea is
+            that `estimating_equation(input_base, hyper_base) = [0, ..., 0]`.
+        solver : `callable`
+            A function of a single vector variable `v` solving :math:`H^{-1} v`,
+            where `H` is the Hessian of the estimating equation with respect
+            to the input variable at `input_base`, `hyper_base`.
+        input_base : `numpy.ndarray`
+            The base value of the parameter to be optimized
+        hyper_base : `numpy.ndarray`
+            The base value of the hyperparameter.
+        """
 
         self._input_base = deepcopy(input_base)
         self._hyper_base = deepcopy(hyper_base)
         self._solver = solver
 
         def estimating_equation_lagrange(ipar, hpar, lam):
-            return \
-                estimating_equation(ipar, hpar) + \
-                np.dot(lam, ipar - self._input_base)
+            return estimating_equation(ipar, hpar) + lam
         self.estimating_equation_lagrange = estimating_equation_lagrange
 
-        self._obj = lambda ipar: estimating_equation(ipar, self._hyper_base)
-        self._obj_grad = autograd.grad(self._obj)
+        # self._obj = lambda ipar: estimating_equation(ipar, self._hyper_base)
+        # self._obj_grad = autograd.jacobian(self._obj)
 
-        self._lam_base = -1 * self._obj_grad(self._input_base)
+        #self._lam_base = -1 * self._obj_grad(self._input_base)
+        self._lam_base = \
+            -1 * estimating_equation(self._input_base, self._hyper_base)
         self._dlam = -1 * self._lam_base
 
         self._cross_sens = CrossSensitivity(
@@ -144,16 +163,28 @@ class OptimumChecker():
             term_12=False)
 
     def get_newton_step(self):
+        """Return a Netwon step towards the optimum.
+        """
         return self._cross_sens.get_di2(self._dlam)
 
     def get_dinput_dhyper(self, dhyper):
-        return self._cross_sens.get_di2(dhyper)
+        """Return the first directional derivative of the optimum with respect
+        to the hyperparameter in the direction `dhyper`.
+        """
+        return self._cross_sens.get_di1(dhyper)
 
     def evaluate(self, hyper_new, dinput_dhyper=None, newton_step=None):
+        """Return the first-order approximation to the change in
+        dinput_dhyper as you take a Newton step.
+        """
         dhyper = hyper_new - self._hyper_base
         if dinput_dhyper is None:
             dinput_dhyper = self.get_dinput_dhyper(dhyper)
         if newton_step is None:
             newton_step = self.get_newton_step()
-        return self._cross_sens.evaluate(
-            dhyper, dlam, di1=dinput_dhyper, di2=newton_step) + hyper_new
+        dinput_dhyper_correction = \
+            self._cross_sens.evaluate(
+                dhyper, self._dlam,
+                di1=dinput_dhyper,
+                di2=newton_step)
+        return self._input_base + dinput_dhyper + dinput_dhyper_correction
